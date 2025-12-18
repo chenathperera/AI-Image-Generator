4dd147ccd7870c8ff3b9cccf3fac5f791a619139
Chenath Perera <chenathperera@gmail.com>
2025-12-18 18:50:03 +0530

    frontend setup

---
+++ client/src/assets/assets.js
@@ -0,0 +1,64 @@
+
+
+export const stepsData = [
+    {
+        title: 'Describe Your Vision',
+        description: 'Type a phrase, sentence, or paragraph that describes the image you want to create',
+        icon: step_icon_1 ,
+    },
+    {
+        title: 'Watch the Magic',
+        description: 'Our AI-Powered engine will transform you text into a high-quality, unique image in seconds.',
+        icon: step_icon_2 ,
+    },
+    {
+        title: 'Dowload & Share',
+        description: 'Instantly download you creation or share it with the world directly from our platform',
+        icon: step_icon_3 ,
+    },
+]
+
+export const testimonialsData = [
+    {
+        image: profile_img_1,
+        name:'Donald jackman',
+        role:'Graphic Designer',
+        stars:5,
+        text:`balalalal`
+    },
+    {
+         image: profile_img_1,
+        name:'Donald jackman',
+        role:'Graphic Designer',
+        stars:5,
+        text:`balalalal`
+    },
+    {
+         image: profile_img_1,
+        name:'Donald jackman',
+        role:'Graphic Designer',
+        stars:5,
+        text:`balalalal`
+    },
+]
+
+export const plans = [
+    {
+        id:'Basic',
+        price:10,
+        credits:100,
+        desc:'Best for personal use',
+    },
+    {
+        id:'Advanced',
+        price:50,
+        credits:500,
+        desc:'Best for business use',
+    },
+    {
+         id:'Business',
+        price:250,
+        credits:5000,
+        desc:'Best for enterprice use',
+    },
+]
