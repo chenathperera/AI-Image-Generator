84c4cfe9d0e883ad9eb6ceeec3ac7a035d3000aa
Chenath Perera <chenathperera@gmail.com>
2025-12-20 20:34:09 +0530

    db connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from "./config/mongodb.js"
 
 const PORT = process.env.PORT || 4000
 const app = express()
+connectDB();
 
 app.use(express.json())
 app.use(cors())
